46251f49bc160b4c443954a113e8f5b057abbea6
Olga Verbetska <verbetska150@gmail.com>
2021-05-09 22:41:01 +0300

    create public&private routes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-import { Component, Suspense, lazy} from 'react';
+import { Component, Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
-
 import routes from './routes';
-import {authOperations} from './redux/auth'
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
-import AppBar from './components/AppBar'
+import { authOperations } from './redux/auth';
+import AppBar from './components/AppBar';
 
 import './styles.css';
 
-
-
-const HomeView = lazy(()=> import('./views/HomeView' /* webpackChunkName: "home-page" */));
-const RegisterView = lazy(()=> import('./views/RegisterView' /* webpackChunkName: "register-page" */));
-const LoginView = lazy(()=> import ('./views/LoginView') /* webpackChunkName: "login-page" */);
-const ContactsView = lazy(()=> import ('./views/ContactsView' /* webpackChunkName: "contacts-page */));
-const NotFoundView = lazy(()=> import ('./views/NotFoundView' /* webpackChunkName: "not-found-page */));
-
-
-
+const HomeView = lazy(() =>
+  import('./views/HomeView' /* webpackChunkName: "home-page" */),
+);
+const RegisterView = lazy(() =>
+  import('./views/RegisterView' /* webpackChunkName: "register-page" */),
+);
+const LoginView = lazy(
+  () => import('./views/LoginView') /* webpackChunkName: "login-page" */,
+);
+const ContactsView = lazy(() =>
+  import('./views/ContactsView' /* webpackChunkName: "contacts-page */),
+);
+const NotFoundView = lazy(() =>
+  import('./views/NotFoundView' /* webpackChunkName: "not-found-page */),
+);
 
 class App extends Component {
-  componentDidMount (){
+  componentDidMount() {
     this.props.onRefreshUser();
   }
   render() {
     return (
       <div className="container">
-        <AppBar/>
-         <Suspense fallback = {<h1>Loading...</h1>}>
-        <Switch>
-          <Route path={routes.home} exact component={HomeView} />
-          <RegisterView path={routes.register} component={RegisterView} />
-          <LoginView path={routes.login} component={LoginView} />
-          <ContactsView path={routes.contacts} component={ContactsView} />
-          <Route component = {NotFoundView}/>
-         
-        </Switch>
+        <AppBar />
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Switch>
+            <Route path={routes.home} exact component={HomeView} />
+            <PublicRoute path={routes.register} component={RegisterView} redirectTo = {routes.contacts} restricted/>
+            <PublicRoute path={routes.login} component={LoginView} redirectTo = {routes.contacts} restricted/>
+            <PrivateRoute path={routes.contacts} component={ContactsView} redirectTo = {routes.login}/>
+            <Route component={NotFoundView} />
+          </Switch>
         </Suspense>
-
-       
       </div>
     );
   }
 }
 
 const mapDispatchToProps = {
-   
   onRefreshUser: authOperations.getCurrentUser,
-
-}
+};
 
 export default connect(null, mapDispatchToProps)(App);
-// const mapDispatchToProps = dispatch => ({
-//   onSubmit: data => dispatch(contactsOperations.addContact(data)),
-// });
--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,34 +2,48 @@ import routes from '../routes';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import {authSelectors} from '../redux/auth'
+import { authSelectors } from '../redux/auth';
 
 import UserMenu from '../components/UserMenu';
-import '../../src/styles.css'
+import '../../src/styles.css';
 
+const AppBar = ({ isAuth }) => (
+  <div className="AppBar">
+    {isAuth ? (
+      <UserMenu />
+    ) : (
+      <ul>
+        {' '}
+        <NavLink
+          to={routes.register}
+          className="linkHome"
+          activeClassName="active-linkHome"
+        >
+          Register
+        </NavLink>
+        <NavLink
+          to={routes.login}
+          className="linkHome"
+          activeClassName="active-linkHome"
+        >
+          Login
+        </NavLink>
+      </ul>
+    )}
 
-const AppBar = ({isAuth}) => (
-  <div className = "AppBar">
-
-   
- {isAuth ?  <UserMenu/> : (<ul> <NavLink to={routes.register}  className="linkHome"  activeClassName="active-linkHome" >
-        Register
-      </NavLink>
-      <NavLink to={routes.login}  className="linkHome" activeClassName="active-linkHome" >
-        Login
-      </NavLink></ul>)}  
-
-    <ul>
-   
-      <NavLink to={routes.contacts}  className="linkHome" activeClassName="active-linkHome" >
+    {isAuth && (
+      <NavLink
+        to={routes.contacts}
+        className="linkHome"
+        activeClassName="active-linkHome"
+      >
         Contacts
       </NavLink>
-
-    </ul>
-    </div>
+    )}
+  </div>
 );
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   isAuth: authSelectors.getIsAuthenticated(state),
 });
 
