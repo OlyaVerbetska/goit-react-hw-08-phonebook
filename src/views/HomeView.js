c46d1641ac83ecb2385365271ad8cf6aa8937729
Olga Verbetska <verbetska150@gmail.com>
2021-05-10 19:26:47 +0300

    fix

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,17 +1,3 @@
-import { NavLink } from 'react-router-dom';
-import routes from '../routes';
-
-const HomeView = () => (
-    <div>
-  
-    <h1>Home Page</h1>
-    <p>To access the functionality of this application, please <NavLink to={routes.login}className = "linkHome"  activeClassName="active-linkHome">
-login
-</NavLink> or <NavLink to={routes.register} className = "linkHome" activeClassName="active-linkHome">
-register
-</NavLink></p>
-    </div>
- 
-);
+const HomeView = () => <h1>Home Page</h1>;
 
 export default HomeView;
